fb266b81441e707ec7483ceacb210d06bb63b680
刘谨华 <3104587339@qq.com>
2019-01-17 19:57:12 +0800

    第二次上传

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
 int main(){
+    printf("huanyingjinruzhuanchangweizhiguanlixitong\n");
+    while(1){
+        printf("1---tianjiacheliangweizhi\n");
+        printf("2---shanchucheliangweizhi\n");
+        printf("3---xianshiquanbucheliangweizhi\n");
+        printf("4---fanhuiliwodeweizhizuijindecheliang\n");
+        printf("5---tuichu\n");
 
-    printf("hello world!");
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
 
     return 0;
-
+    }
 }
