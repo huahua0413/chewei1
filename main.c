3c6240048063e13d02c8b6bbb10ba43ab6c2149e
刘谨华 <3104587339@qq.com>
2019-01-17 20:01:33 +0800

    第三次上传

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+struct point{
+    int x;
+    int y;
+};
 int main(){
     printf("huanyingjinruzhuanchangweizhiguanlixitong\n");
     while(1){
+        int index = 0;
+        struct point arr[100];
         printf("1---tianjiacheliangweizhi\n");
         printf("2---shanchucheliangweizhi\n");
         printf("3---xianshiquanbucheliangweizhi\n");
@@ -11,6 +17,20 @@ int main(){
         printf("qingxuanzexiangyinggongneng\n");
         int code;
         scanf("%d",&code);
+         if(code == 1){
+            printf("qingshurucheliangweizhidexzuobiaoheyzuobiao: \n");
+            int x;
+            int y;
+            scanf("%d",&x);
+            scanf("%d",&x);
+            arr[index].x = x;
+            arr[index].y = y;
+            index++;
+            printf("tianjiawancheng,dianjihuichejixu\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
+        }
 
     return 0;
     }
